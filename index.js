eb6a9d63283066637e95b10e6c30946c41384966
Azim-Ul-Karim <azimulkarim.ruet@gmail.com>
2025-11-13 21:09:35 +0600

    Updated

--- index.js
+++ index.js
@@ -4,3 +4,24 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nxdef89.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Local Food Lovers Network!')
+})
+
+app.listen(port, () => {
+    console.log(`LFLN is using port ${port}!`);
+})
