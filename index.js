7c29062cc5883782acd6316e25cf4f5f348eb1cc
Azim-Ul-Karim <azimulkarim.ruet@gmail.com>
2025-11-13 21:12:50 +0600

    Updated

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await reviewsCollection.findOne(query);
+            res.send(result);
+        })
+
+        
     } finally {
         // await client.close();
     }
